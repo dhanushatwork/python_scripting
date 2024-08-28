ab4baf4289d7b7e9b4a9507d45640e5fd483cafc
dhanushatwork <dhanush97atwork@gmail.com>
2024-08-29 02:00:33 +0530

    k8s files

---
+++ Day-1/datatypes.py
@@ -0,0 +1,29 @@
+x = 5
+print(type(x))
+
+x = 3.16
+print(type(x))
+
+x = "Hello world"
+print(type(x))
+
+x = ["kube","Docker","Ansible"]
+print(type(x))
+
+x = ("Git", "Jenkins")
+print(type(x))
+
+x = True
+print(type(x))
+
+x = {"Name":"Big Dawgs","Role":"Song writer"}
+print(type(x))
+
+
+# <class 'int'>
+# <class 'float'>
+# <class 'str'>
+# <class 'list'>
+# <class 'tuple'>
+# <class 'bool'>
+# <class 'dict'>
